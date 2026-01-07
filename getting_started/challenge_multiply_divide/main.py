041af91108d1bb0232eaebf89ccdd10f78f33728
akdevelops-creates <>
2026-01-07 22:58:21 +0000

    Challenge: Multiply & Divide

---
+++ getting_started/challenge_multiply_divide/main.py
@@ -0,0 +1,13 @@
+# MULTIPLICATION
+# Calculate tax value by $ of peanut butter and the tax RATE
+peanut_butter_price = 7.32
+tax_rate = 0.2
+tax = peanut_butter_price * tax_rate
+print(f"{tax}")
+
+# DIVISION 
+# Calculate the price of a single egg
+number_of_eggs = 20
+total_eggs_price = 8.30
+cost_per_egg = total_eggs_price / number_of_eggs
+print(f"{cost_per_egg}") 
