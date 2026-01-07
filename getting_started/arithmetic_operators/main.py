8198cc67132e3314486c12df2a49c55186d0a0bc
akdevelops-creates <>
2026-01-07 21:08:06 +0000

    Arithmetic Operators

---
+++ getting_started/arithmetic_operators/main.py
@@ -0,0 +1,11 @@
+#List variables and calculate addition 
+price_of_milk = 5.42
+price_of_bread = 4.39
+gift_card = 5.50
+total_price = 12.30
+milk_bread = price_of_milk + price_of_bread
+print(milk_bread)
+
+#Calculate subtraction to see the value of the gift card
+val_card = total_price - gift_card 
+print(val_card)
